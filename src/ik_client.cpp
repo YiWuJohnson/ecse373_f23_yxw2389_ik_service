dd332589615b46fd2b60a4859d1b6615c4c47c95
YiWuJohnson <yxw2389@case.edu>
2023-12-07 15:31:53 -0500

    modify

--- src/ik_client.cpp
+++ src/ik_client.cpp
@@ -1,5 +1,6 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
+#include "geometry_msgs/Pose.h"
 #include "ik_service/PoseIK.h"
 #include "ur_kinematics/ur_kin.h"
 
@@ -13,15 +14,15 @@ int main(int argc, char  *argv[])
     ros::ServiceClient client = n.serviceClient<ik_service::PoseIK>("pose_ik");
 
     // Declare a variable
-    ik_service::PoseIK ik_pose;
+    ik_service::PoseIK pose_ik;
     // Declare a Pose variable
     geometry_msgs::Pose part_pose;
     // Set the variables
     part_pose.position.x = 0.5;
-    ik_pose.request = part_pose;
+    pose_ik.request.part_pose = part_pose;
     if (client.call(pose_ik))
     {
-       ROS_INFO("Call to ik_service returned [%i] solutions",     pose_ik.res.num_sols);
+       ROS_INFO("Call to ik_service returned [%i] solutions",     pose_ik.response.num_sols);
     }
      else
     {
